9d57c2686934199a10a73e73c276fa69d23c63e4
dealer <nguyenkhanhsl1997@gmail.com>
2022-03-25 13:55:25 +0700

    insert paragraph after insert table

--- @ckeditor/ckeditor5-table/src/commands/inserttablecommand.js
+++ @ckeditor/ckeditor5-table/src/commands/inserttablecommand.js
@@ -7,8 +7,8 @@
  * @module table/commands/inserttablecommand
  */
 
-import { Command } from 'ckeditor5/src/core';
-import { findOptimalInsertionRange } from 'ckeditor5/src/widget';
+import { Command } from "ckeditor5/src/core";
+import { findOptimalInsertionRange } from "ckeditor5/src/widget";
 
 /**
  * The insert table command.
@@ -22,60 +22,67 @@ import { findOptimalInsertionRange } from 'ckeditor5/src/widget';
  * @extends module:core/command~Command
  */
 export default class InsertTableCommand extends Command {
-	/**
-	 * @inheritDoc
-	 */
-	refresh() {
-		const model = this.editor.model;
-		const selection = model.document.selection;
-		const schema = model.schema;
+  /**
+   * @inheritDoc
+   */
+  refresh() {
+    const model = this.editor.model;
+    const selection = model.document.selection;
+    const schema = model.schema;
 
-		this.isEnabled = isAllowedInParent( selection, schema );
-	}
+    this.isEnabled = isAllowedInParent(selection, schema);
+  }
 
-	/**
-	 * Executes the command.
-	 *
-	 * Inserts a table with the given number of rows and columns into the editor.
-	 *
-	 * @param {Object} options
-	 * @param {Number} [options.rows=2] The number of rows to create in the inserted table.
-	 * @param {Number} [options.columns=2] The number of columns to create in the inserted table.
-	 * @param {Number} [options.headingRows] The number of heading rows.
-	 * If not provided it will default to {@link module:table/table~TableConfig#defaultHeadings `config.table.defaultHeadings.rows`}
-	 * table config.
-	 * @param {Number} [options.headingColumns] The number of heading columns.
-	 * If not provided it will default to {@link module:table/table~TableConfig#defaultHeadings `config.table.defaultHeadings.columns`}
-	 * table config.
-	 * @fires execute
-	 */
-	execute( options = {} ) {
-		const model = this.editor.model;
-		const selection = model.document.selection;
-		const tableUtils = this.editor.plugins.get( 'TableUtils' );
-		const config = this.editor.config.get( 'table' );
+  /**
+   * Executes the command.
+   *
+   * Inserts a table with the given number of rows and columns into the editor.
+   *
+   * @param {Object} options
+   * @param {Number} [options.rows=2] The number of rows to create in the inserted table.
+   * @param {Number} [options.columns=2] The number of columns to create in the inserted table.
+   * @param {Number} [options.headingRows] The number of heading rows.
+   * If not provided it will default to {@link module:table/table~TableConfig#defaultHeadings `config.table.defaultHeadings.rows`}
+   * table config.
+   * @param {Number} [options.headingColumns] The number of heading columns.
+   * If not provided it will default to {@link module:table/table~TableConfig#defaultHeadings `config.table.defaultHeadings.columns`}
+   * table config.
+   * @fires execute
+   */
+  execute(options = {}) {
+    const model = this.editor.model;
+    const selection = model.document.selection;
+    const tableUtils = this.editor.plugins.get("TableUtils");
+    const config = this.editor.config.get("table");
 
-		const insertionRange = findOptimalInsertionRange( selection, model );
+    const insertionRange = findOptimalInsertionRange(selection, model);
 
-		const defaultRows = config.defaultHeadings.rows;
-		const defaultColumns = config.defaultHeadings.columns;
+    const defaultRows = config.defaultHeadings.rows;
+    const defaultColumns = config.defaultHeadings.columns;
 
-		if ( options.headingRows === undefined && defaultRows ) {
-			options.headingRows = defaultRows;
-		}
+    if (options.headingRows === undefined && defaultRows) {
+      options.headingRows = defaultRows;
+    }
 
-		if ( options.headingColumns === undefined && defaultColumns ) {
-			options.headingColumns = defaultColumns;
-		}
+    if (options.headingColumns === undefined && defaultColumns) {
+      options.headingColumns = defaultColumns;
+    }
 
-		model.change( writer => {
-			const table = tableUtils.createTable( writer, options );
+    model.change((writer) => {
+      const table = tableUtils.createTable(writer, options);
 
-			model.insertContent( table, insertionRange );
+      model.insertContent(table, insertionRange);
 
-			writer.setSelection( writer.createPositionAt( table.getNodeByPath( [ 0, 0, 0 ] ), 0 ) );
-		} );
-	}
+      // After insert table => insert paragraph after
+      this.editor.execute("insertParagraph", {
+        position: this.editor.model.createPositionAfter(table),
+      });
+
+      writer.setSelection(
+        writer.createPositionAt(table.getNodeByPath([0, 0, 0]), 0)
+      );
+    });
+  }
 }
 
 // Checks if the table is allowed in the parent.
@@ -83,9 +90,12 @@ export default class InsertTableCommand extends Command {
 // @param {module:engine/model/selection~Selection|module:engine/model/documentselection~DocumentSelection} selection
 // @param {module:engine/model/schema~Schema} schema
 // @returns {Boolean}
-function isAllowedInParent( selection, schema ) {
-	const positionParent = selection.getFirstPosition().parent;
-	const validParent = positionParent === positionParent.root ? positionParent : positionParent.parent;
+function isAllowedInParent(selection, schema) {
+  const positionParent = selection.getFirstPosition().parent;
+  const validParent =
+    positionParent === positionParent.root
+      ? positionParent
+      : positionParent.parent;
 
-	return schema.checkChild( validParent, 'table' );
+  return schema.checkChild(validParent, "table");
 }
